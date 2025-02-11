1f956549882e82a9a5c3186c608e42f140c464bb
Michael (Mo) Morando <mikemojr@gmail.com>
2025-02-11 13:32:42 -0800

    Remove work-in-progress files and directories. Move to dev branch 'dev_annotations'

--- data/annotations/scripts/auid_mapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-### makes key specific to old version of nifHDB
-# analysis/Jmags/mappingOLD2NEWauids/fastaMapper.sh current.asv2auid.fasta auid.filteredRA.fasta >fasta_id_map2.tsv
-# shell realpath"data/annotations/scripts/fastaMapper.sh"
-# "~/mmorando@ucsc.edu - Google Drive/My Drive/data/amplicon_review/all_studies/master_fasta/nifHDB/auid.filtered.nifHDB.fasta"
-### makes key specific to nifHDB
-# fastaMapper.sh current.asv2auid.fasta auid.filtered.nifHDB.fasta >nifHDB_old2new_auids_map.tsv
-
-/Users/mo/Projects/nifH_amp_project/myWork/data/annotations/scripts/fastaMapper.sh ~/mmorando@ucsc.edu - Google Drive/My Drive/data/amplicon_review/all_studies/master_fasta/nifHDB/auid.filtered.nifHDB.fasta
